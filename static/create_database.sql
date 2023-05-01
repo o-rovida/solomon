b29a82673a362593bbf2b610407e0c7d5f562ae0
joao <jprfsousa@gmail.com>
2023-05-01 17:52:23 -0300

    *

--- static/create_database.sql
+++ static/create_database.sql
@@ -6,23 +6,36 @@ CREATE TABLE book (
     testament CHAR(2) NOT NULL -- VT para Velho Testamento ou NT para Novo Testamento
 );
 
-CREATE TABLE author (
-	authorName VARCHAR(255) PRIMARY KEY,
-    authorBiography VARCHAR(255)
-);
-
 CREATE TABLE verse (
-	bookAbbreviation CHAR(2) NOT NULL,
+	verseId SMALLINT AUTO_INCREMENT PRIMARY KEY,
+    bookAbbreviation CHAR(2) NOT NULL,
     chapterNumber SMALLINT NOT NULL,
-    chapterAuthor VARCHAR(255),
     verseNumber SMALLINT NOT NULL,
     verseContent VARCHAR(1020) NOT NULL UNIQUE,
-    PRIMARY KEY(bookAbbreviation,chapterNumber,verseNumber),
-    FOREIGN KEY (bookAbbreviation) REFERENCES book(bookAbbreviation),
-    FOREIGN KEY (chapterAuthor) REFERENCES author(authorName)
+    FOREIGN KEY (bookAbbreviation) REFERENCES book(bookAbbreviation)
+);
+
+CREATE TABLE quote (
+    quoteId SMALLINT AUTO_INCREMENT PRIMARY KEY,
+    quoteReference VARCHAR(15)
+);
+
+CREATE TABLE quoteVerse (
+    quoteId SMALLINT,
+    verseId SMALLINT,
+    PRIMARY KEY(quoteId,verseId),
+    FOREIGN KEY (quoteId) REFERENCES quote(quoteId)
+    FOREIGN KEY (verseId) REFERENCES verse(verseId)
 );
 
 CREATE TABLE comment (
+    commentId SMALLINT AUTO_INCREMENT PRIMARY KEY,
+    quoteId SMALLINT, FOREIGN KEY (quoteId) REFERENCES quote(quoteId),
+    commentContent VARCHAR(8000) NOT NULL UNIQUE 
+);
 
-)
-
+CREATE TABLE ref (
+    refId SMALLINT AUTO_INCREMENT PRIMARY KEY,
+    quote1Id SMALLINT, FOREIGN KEY (quote1Id) REFERENCES quote(quoteId),
+    quote2Id SMALLINT, FOREIGN KEY (quote2Id) REFERENCES quote(quoteId)
+);
